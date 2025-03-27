9b6e98c8fa94c66bcf4cf02bef7ddeca8c96ac7f
Vitor Albertini <v206645@dac.unicamp.br>
2025-03-27 18:39:18 -0300

    Robo Combustivel pronto e comentado

--- semanais/src/lab02/robos/robos_terrestres/RoboCombustivel.java
+++ semanais/src/lab02/robos/robos_terrestres/RoboCombustivel.java
@@ -4,13 +4,22 @@ import lab02.Ambiente;
 import lab02.robos.RoboTerrestre;
 
 public class RoboCombustivel extends RoboTerrestre {
-    private int combustivel;
+
+    // Subclasse para lidar com robos que utilizam combustivel para locoção
+
+    private int combustivel;    // Se assume que sempre possuem um tanque suficiente para o combustivel
+
+
+    // Contrutor do robo Combustivel, considera o combustivel presente no robo (minimo de zero)
 
     public RoboCombustivel(String nome, int posX, int posY, String direcao, int raioSensor, int velocidadeMaxima, int combustivel) {
         super(nome, posX, posY, direcao, raioSensor, velocidadeMaxima);
-        this.combustivel = combustivel;
+        this.combustivel = Math.min(0, combustivel);
 
     }
+
+
+    // Setters e Getters:
 
     public int getCombustivel() {
         return combustivel;
@@ -20,18 +29,26 @@ public class RoboCombustivel extends RoboTerrestre {
         this.combustivel = combustivel;
     }
 
+
+    // Metodo para abastecer o robo:
+
     public void abastecer(int gasolina) {
-        if (gasolina >= 0)
+        if (gasolina >= 0)      // Não se remove gasolina do robo no abastecimento
             setCombustivel(getCombustivel() + gasolina);
         else
             System.out.printf("Gasolina de %s precisa ser maior que 0\n", getNome());
 
     }
 
+
+    // Sobreescrita do metodo Mover do robo Terrestre para acomodar o uso de gasolina
+    // O robo precisa ter no minimo uma quantia igual à soma das das coordenadas movidas unidirecionalmente
+    // Ou seja, se ele se move 3 em X e -10 em Y, consome 13 de gasolina (3 + 10)
+
     @Override
     public void mover(int deltaX, int deltaY, Ambiente ambiente) {
 
-        if ((Math.abs(deltaX) + Math.abs(deltaY)) < getCombustivel()) {
+        if ((Math.abs(deltaX) + Math.abs(deltaY)) <= getCombustivel()) {
             setCombustivel(getCombustivel() - Math.abs(deltaX) - Math.abs(deltaY));
             super.mover(deltaX, deltaY, ambiente);
 
