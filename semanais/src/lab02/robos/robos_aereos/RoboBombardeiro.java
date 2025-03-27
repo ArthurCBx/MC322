c290988e1e9f73f90b632cdf34ac1e86209bc6ff
Arthur Barreto <arthur.cb102003@gmail.com>
2025-03-27 19:10:58 -0300

    as

--- semanais/src/lab02/robos/robos_aereos/RoboBombardeiro.java
+++ semanais/src/lab02/robos/robos_aereos/RoboBombardeiro.java
@@ -7,6 +7,14 @@ import lab02.robos.RoboAereo;
 import java.util.ArrayList;
 import java.util.List;
 
+/* RoboBombardeiro é uma subclasse de RoboAereo que tem a característica específica de destruir os robos que estão na mesma posição (x,y) que ele e na mesma altitude ou inferior.
+Caso o RoboBombardeiro esteja na altitude 0, ele também é destruído.
+
+Atributos:
+Bombas: quantidade de bombas que o RoboBombardeiro possui.
+CapacidadeBombas: quantidade máxima de bombas que o RoboBombardeiro pode carregar.
+* */
+
 public class RoboBombardeiro extends RoboAereo {
     private int bombas;
     private final int capacidadeBombas;
@@ -17,7 +25,7 @@ public class RoboBombardeiro extends RoboAereo {
         this.capacidadeBombas = Math.abs(capacidadeBombas);
 
     }
-
+    // Getters e Setters específicos do bombardeiro
     public int getBombas() {
         return bombas;
     }
@@ -26,10 +34,13 @@ public class RoboBombardeiro extends RoboAereo {
         return capacidadeBombas;
     }
 
-    public void setBombas(int bombas) {
+    // Set como protected porque nao deve ser acessado fora desta classe ou subclasse.
+    // Há metodo específico para definir bombas.
+    protected void setBombas(int bombas) {
         this.bombas = bombas;
     }
 
+    // Adiciona bombas ao RoboBombardeiro, verificando se a capacidade limite foi atingida. Não pode ser negativo.
     public void carregarBombas(int bombas) {
         if (bombas >= 0)
             setBombas(Math.min(getBombas() + bombas, getCapacidadeBombas()));
@@ -38,8 +49,10 @@ public class RoboBombardeiro extends RoboAereo {
 
     }
 
+    // Metodo que bombardeia robos na mesma posição (x,y) e altitude igual ou inferior.
     public void bombardear(Ambiente ambiente) {
 
+        // Não pode bombardear se o robo estiver sem bombas.
         if (getBombas() <= 0) {
             System.out.printf("O robo %s não tem bombas\n", getNome());
             return;
