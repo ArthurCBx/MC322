6b5ac45cf412d9b78860e01af72707e7dff44cb3
Vitor Albertini <v206645@dac.unicamp.br>
2025-03-27 18:29:40 -0300

    Robo Terrestre pronto e comentado

--- semanais/src/lab02/robos/RoboTerrestre.java
+++ semanais/src/lab02/robos/RoboTerrestre.java
@@ -3,13 +3,22 @@ package lab02.robos;
 import lab02.Ambiente;
 
 public class RoboTerrestre extends Robo {
+
+    // Subclasse para implementar um robo que é exclusivamente terrestre e possui uma velocidade maxima
+
     private int velocidadeMaxima;
+
+
+    // Metodo construtor para o robo Terrestre que considera a velocidade maxima (em modulo)
 
     public RoboTerrestre(String nome, int posX, int posY, String direcao, int raioSensor, int velocidadeMaxima) {
         super(nome, posX, posY, direcao, raioSensor);
-        this.velocidadeMaxima = Math.abs(velocidadeMaxima);
+        this.velocidadeMaxima = Math.abs(velocidadeMaxima); // Velocidade maxima sempre positiva
 
     }
+
+
+    // Setters e Getters:
 
     public int getVelocidadeMaxima() {
         return velocidadeMaxima;
@@ -19,8 +28,12 @@ public class RoboTerrestre extends Robo {
         this.velocidadeMaxima = velocidadeMaxima;
     }
 
+
+    // Sobreescrita do metodo Mover para o robo Terrestre:
+    // A velocidade maxima é considerada unidimensional, ou seja, pode se mover uma velocidade maxima para cada direção
+
     @Override
-    public void mover(int deltaX, int deltaY, Ambiente ambiente) {             // Velocidade maxima unidimencional
+    public void mover(int deltaX, int deltaY, Ambiente ambiente) {
         if (Math.abs(deltaX) > getVelocidadeMaxima() || Math.abs(deltaY) > getVelocidadeMaxima())
             System.out.printf("O movimento de %s excede a velocidade maxima, operação cancelada\n", getNome());
         else
