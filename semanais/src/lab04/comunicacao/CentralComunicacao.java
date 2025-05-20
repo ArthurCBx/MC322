153750a6ec168002d860adbe72d4cb2caf6b72fc
Arthur Barreto <arthur.cb102003@gmail.com>
2025-05-20 16:16:50 -0300

    Deixando criada a classe CentralComunicacao

--- semanais/src/lab04/comunicacao/CentralComunicacao.java
+++ semanais/src/lab04/comunicacao/CentralComunicacao.java
@@ -1,4 +1,17 @@
 package lab04.comunicacao;
 
+import java.util.ArrayList;
+
 public class CentralComunicacao {
+    private ArrayList<String> mensagens = new ArrayList<String>();
+    public CentralComunicacao(){}
+
+    public void registrarMensagem(String remetente, String msg){
+        mensagens.add(remetente + ": " + msg);
+    }
+    public void exibirMensagens(){
+        for (String msg : mensagens) {
+            System.out.println(msg);
+        }
+    }
 }
